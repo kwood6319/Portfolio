9c0224ad4ab5e9232648426dca36d69d3f8680f6
Katie Wood <kwood6319@hotmail.co.uk>
2026-04-23 18:18:46 +0900

    Added columns for images to project and stage tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_23_090204) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_23_091705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_stat_statements"
@@ -43,6 +43,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_23_090204) do
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "description"
+    t.string "image"
     t.string "link"
     t.string "role"
     t.string "skills", default: [], array: true
@@ -54,6 +55,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_23_090204) do
   create_table "stages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
+    t.string "image"
     t.bigint "project_id", null: false
     t.string "title"
     t.datetime "updated_at", null: false
