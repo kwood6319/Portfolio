cd6bc742551364aa0db7b02a7129544595cfd9f0
Katie Wood <kwood6319@hotmail.co.uk>
2026-04-23 18:15:42 +0900

    Finished config for mobility on projects level

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
