66987c2843a81e36332f23c292b87b87bccbbe47
Katie Wood <kwood6319@hotmail.co.uk>
2026-04-23 19:39:08 +0900

    Added translations to stage model

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -1,4 +1,9 @@
 class Stage < ApplicationRecord
   belongs_to :project
   validates :title, inclusion: { in: %w[Design Schema Wireframing Development Testing Future\ Changes] }
+
+  extend Mobility
+
+  translates :title, type: :string
+  translates :description, type: :text
 end
